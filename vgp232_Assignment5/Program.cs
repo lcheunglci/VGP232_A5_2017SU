4675276e3011239392de8bb0dc69584bbef1b7cb
Lawrence Cheung <lcheunglci@gmail.com>
2017-09-05 07:35:55 -0700

    add character manager
    used for managing enemies, npcs and the player and initialize it in the main program

--- vgp232_Assignment5/Program.cs
+++ vgp232_Assignment5/Program.cs
@@ -15,8 +15,19 @@ namespace vgp232_Assignment5
         {
             Console.WriteLine("Initializing Systems...");
             // TODO: create inventory system
-            // TODO: create character system
+
+            Console.WriteLine("Initializing Character Manager");
+            Character.Character player = new Character.Character(100, Race.Elf, Alignment.Neutral);
+            CharacterManager characterManager = CharacterManager.Instance;
+            characterManager.Player = player;
+            Character.Character enemyOne = new Character.Character(50, Race.Orc, Alignment.ChaoticEvil);
+            Character.Character enemyTwo = new Character.Character(50, Race.Orc, Alignment.NeutralEvil);
+            characterManager.AddEnemy(enemyOne);
+            characterManager.AddEnemy(enemyTwo);
+
             // TODO: create persistent data system
+
+            Console.ReadKey();
 
         }
     }
