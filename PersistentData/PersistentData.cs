9cd4368da97590a5b14820508a47f40de4a3888a
Lawrence Cheung <lcheunglci@gmail.com>
2017-09-05 06:55:48 -0700

    Add xml serialization
    Created a serialize and deserialize method in the persistent data class library

--- PersistentData/PersistentData.cs
+++ PersistentData/PersistentData.cs
@@ -8,5 +8,22 @@ namespace PersistentData
 {
     public class PersistentData
     {
+        public static void Serialize<T>(T data, string path = "saveData.xml")
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (TextWriter writer = new StreamWriter(path))
+            {
+                serializer.Serialize(writer, data);
+            }
+        }
+
+        public static T Deserialize<T>(string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (TextReader reader = new StreamReader(path))
+            {
+                return (T)serializer.Deserialize(reader);
+            }
+        }
     }
 }
