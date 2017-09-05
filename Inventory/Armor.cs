d06d5e29687998e31ab36e943ea6b68749a768f2
Lawrence Cheung <lcheunglci@gmail.com>
2017-09-05 07:45:37 -0700

    add armor and weapon to the inventory

---
+++ Inventory/Armor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory
+{
+    public class Armor : Item
+    {
+        int defense;
+
+        public Armor(string name, int modifier, int defense) : base(name, modifier, ItemType.equipment)
+        {
+            this.defense = defense;
+        }
+    }
+}
